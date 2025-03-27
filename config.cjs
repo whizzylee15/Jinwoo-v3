a80129ad40cf5b3cf6fc19f91735bd83eeac461d
whizzylee15 <wizzylee839@gmail.com>
2025-03-27 05:27:18 +0100

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 const config = {
   SESSION_ID: process.env.SESSION_ID || "MALVIN-XD~ExYlhDpS#Obbcx7CEDr-f6odh7Ic8QHseTv_stdnIn2-hbpjHq7k",
-  PREFIX: process.env.PREFIX || '.',
+  PREFIX: process.env.PREFIX || '+',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY !== undefined ? process.env.AUTO_STATUS_REPLY === 'false' : true,
   STATUS_READ_MSG: process.env.STATUS_READ_MSG || '',
